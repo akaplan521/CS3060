2fc05ede21e73e9063432e4cec877ba1da8a30ff
akaplan521 <alexa.kaplan@uvm.edu>
2025-01-26 05:12:46 -0500

    castle thing

--- generate.py
+++ generate.py
@@ -1,5 +1,24 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+length = 1
+width = 1
+height = 1
+x = 0
+y = 0
+z = .5
+pyrosim.Start_SDF("boxes.sdf")
+
+for x in range(5):
+    for y in range(5):
+        length = 1
+        width = 1
+        height = 1
+        for z in range(10):
+            pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+            length *= .90
+            width *= .90
+            height *= .90
+            z += 1
+        y += 1
+    x += 1
 pyrosim.End()
