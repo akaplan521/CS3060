b45780265adb61d7ce0f5927cb89f77fe767ee5f
akaplan521 <alexa.kaplan@uvm.edu>
2025-02-01 20:44:21 -0500

    defining a robot

--- generate.py
+++ generate.py
@@ -6,8 +6,17 @@ height = 1
 x = 0
 y = 0
 z = .5
-pyrosim.Start_SDF("world.sdf")
 
-pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+    pyrosim.End()
 
-pyrosim.End()
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[x,y,z] , size=[length,width,height])
+    pyrosim.End()
+
+
+Create_World()
+Create_Robot()
