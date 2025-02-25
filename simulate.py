09a64482bdbbae481db8ff0dae03d18f540e442f
akaplan521 <alexa.kaplan@uvm.edu>
2025-02-24 20:11:47 -0500

    create class hierarchy

--- simulate.py
+++ simulate.py
@@ -1,50 +1,52 @@
-import pybullet as p
-import time 
-import pybullet_data
-import pyrosim.pyrosim as pyrosim
-import numpy
-import random
-import constants as c
+# import pybullet as p
+# import time 
+# import pybullet_data
+# import pyrosim.pyrosim as pyrosim
+# import numpy
+# import random
+# import constants as c
 
 
-physicsClient = p.connect(p.GUI)
-p.setAdditionalSearchPath(pybullet_data.getDataPath())
-#p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+# physicsClient = p.connect(p.GUI)
+# p.setAdditionalSearchPath(pybullet_data.getDataPath())
+# #p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 
-p.setGravity(0,0,-9.8)
-planeId = p.loadURDF("plane.urdf")
-robotId = p.loadURDF("body.urdf")
+# p.setGravity(0,0,-9.8)
+# planeId = p.loadURDF("plane.urdf")
+# robotId = p.loadURDF("body.urdf")
 
-p.loadSDF("world.sdf")
-pyrosim.Prepare_To_Simulate(robotId)
+# p.loadSDF("world.sdf")
+# pyrosim.Prepare_To_Simulate(robotId)
 
-backLegSensorValues = numpy.zeros(c.ITERATIONS)
-frontLegSensorValues = numpy.zeros(c.ITERATIONS)
+# backLegSensorValues = numpy.zeros(c.ITERATIONS)
+# frontLegSensorValues = numpy.zeros(c.ITERATIONS)
 
-targetAngles = numpy.sin(numpy.linspace(0, 2 * numpy.pi, c.ITERATIONS)) * numpy.pi/4
-#numpy.save('data/sin.npy', targetAngles)
+# targetAngles = numpy.sin(numpy.linspace(0, 2 * numpy.pi, c.ITERATIONS)) * numpy.pi/4
+# #numpy.save('data/sin.npy', targetAngles)
 
-BackLegControl = c.backAmplitude * numpy.sin(c.backFrequency * targetAngles + c.backPhaseOffset)
-FrontLegControl = c.frontAmplitude * numpy.sin(c.frontFrequency * targetAngles + c.frontPhaseOffset)
+# BackLegControl = c.backAmplitude * numpy.sin(c.backFrequency * targetAngles + c.backPhaseOffset)
+# FrontLegControl = c.frontAmplitude * numpy.sin(c.frontFrequency * targetAngles + c.frontPhaseOffset)
 
-for i in range(c.ITERATIONS):
-    p.stepSimulation()
-    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, 
-                                jointName = "Torso_BackLeg", 
-                                controlMode = p.POSITION_CONTROL, 
-                                targetPosition = BackLegControl[i], 
-                                maxForce = c.motorForce)
-    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, 
-                                jointName = "Torso_FrontLeg", 
-                                controlMode = p.POSITION_CONTROL, 
-                                targetPosition = FrontLegControl[i], 
-                                maxForce = c.motorForce)
-    time.sleep(c.sleepTime)
+# for i in range(c.ITERATIONS):
+#     p.stepSimulation()
+#     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+#     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+#     pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, 
+#                                 jointName = "Torso_BackLeg", 
+#                                 controlMode = p.POSITION_CONTROL, 
+#                                 targetPosition = BackLegControl[i], 
+#                                 maxForce = c.motorForce)
+#     pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, 
+#                                 jointName = "Torso_FrontLeg", 
+#                                 controlMode = p.POSITION_CONTROL, 
+#                                 targetPosition = FrontLegControl[i], 
+#                                 maxForce = c.motorForce)
+#     time.sleep(c.sleepTime)
 
-numpy.save('data/sensor.npy', backLegSensorValues)
-numpy.save('data/frontLegSensorValues.npy', frontLegSensorValues)
-p.disconnect()
+# numpy.save('data/sensor.npy', backLegSensorValues)
+# numpy.save('data/frontLegSensorValues.npy', frontLegSensorValues)
+# p.disconnect()
 
+from simulation import SIMULATION
 
+simulation = SIMULATION()
