bed236599237411395e45c0f0cd95a7bc4d61d5d
akaplan521 <alexa.kaplan@uvm.edu>
2025-03-17 23:42:10 -0400

    housekeeping

--- simulation.py
+++ simulation.py
@@ -10,8 +10,8 @@ import pyrosim.pyrosim as pyrosim
 class SIMULATION:
     
     def __init__(self, directOrGUI):
-        
-        if directOrGUI == "DIRECT":
+        self.directOrGUI = directOrGUI
+        if self.directOrGUI == "DIRECT":
             self.physicsClient = p.connect(p.DIRECT)
         else:
             self.physicsClient = p.connect(p.GUI)
@@ -40,7 +40,10 @@ class SIMULATION:
             self.robot.Sense(i)
             self.robot.Think(i)
             self.robot.Act(i)
-            time.sleep(c.sleepTime)
+            if self.directOrGUI == "GUI":
+                time.sleep(0.01)
+            else:
+                time.sleep(c.sleepTime)
 
     def Get_Fitness(self):
         self.robot.Get_Fitness()
